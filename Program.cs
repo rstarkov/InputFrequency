40258967e84ff5594ea970bfa133c45bdfbb3698
rstarkov <sourcecode@roman.st>
2011-12-29 01:10:48 +0000

    Major cleanup. Bugfix for what makes a combo. Save settings manually; dependency on RT.Util removed.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows.Forms;
-using RT.Util;
-using RT.Util.ExtensionMethods;
 
-// don't store key names in settings file (migrate off RT.Util so that others can run this from source?)
-// make sure all key names are good
 // time spent using keyboard
 // mouse
 // time spent using mouse
@@ -17,146 +11,121 @@ namespace InputFrequency
 {
     static class Program
     {
+        static Statistics _stats;
+        static IntPtr _hook;
+
+        static bool[] _keyDown = new bool[256];
+        static DateTime[] _keyDownAt = new DateTime[256];
+        static Key? _lastPressedModifier = null;
+
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
         static void Main()
         {
-            Settings settings;
-            SettingsUtil.LoadSettings(out settings);
+            _stats = Statistics.Load();
+            _hook = WinAPI.SetWindowsHookEx(WinAPI.WH_KEYBOARD_LL, new WinAPI.KeyboardHookProc(HookProc), WinAPI.LoadLibrary("User32"), 0);
 
-            var keyValues = EnumStrong.GetValues<Keys>().Zip(Enum.GetNames(typeof(Keys)), (key, name) => new { key, name }).ToLookup(x => x.key, x => x.name);
-            var names = keyValues.ToDictionary(kgroup => kgroup.Key, kgroup => kgroup.First());
-            var down = new bool[256];
-            var downAt = new DateTime[256];
-            var modifiers = new[] { Keys.LWin, Keys.RWin, Keys.LControlKey, Keys.RControlKey, Keys.LMenu, Keys.RMenu, Keys.LShiftKey, Keys.RShiftKey };
-            Keys? lastPressedKey = null;
-            string previous = null;
-
-            // Fixup some names
-            names[Keys.LControlKey] = "LCtrl";
-            names[Keys.RControlKey] = "RCtrl";
-            names[Keys.LMenu] = "LAlt";
-            names[Keys.RMenu] = "RAlt";
-            names[Keys.LShiftKey] = "LShift";
-            names[Keys.RShiftKey] = "RShift";
-            names[Keys.Snapshot] = "PrintScreen";
-            names[Keys.Scroll] = "ScrollLock";
-            names[Keys.Prior] = "PageUp";
-            names[Keys.Oemplus] = "OemPlus";
-            names[Keys.Oemcomma] = "OemComma";
-            // Add anything missing
-            for (int i = 0; i <= 255; i++)
-                if (!names.ContainsKey((Keys) i))
-                    names[(Keys) i] = "VirtualKey" + i;
-
-            var listener = new GlobalKeyboardListener { HookAllKeys = true };
-
-            listener.KeyDown += (_, e) =>
-            {
-                var key = e.VirtualKeyCode;
-                var ikey = (int) key;
-                if (ikey >= down.Length)
-                {
-                    settings.TooLarge++;
-                    return;
-                }
-                if (!down[ikey])
-                {
-                    downAt[ikey] = DateTime.UtcNow;
-                    lastPressedKey = key;
-                    settings.KeyCounts.IncSafe(names[key]);
-                }
-                down[ikey] = true;
-            };
-            listener.KeyUp += (_, e) =>
-            {
-                var key = e.VirtualKeyCode;
-                var ikey = (int) key;
-                if (ikey >= down.Length)
-                {
-                    settings.TooLarge++;
-                    return;
-                }
-                // alt shift h [H] SHIFT k [K] ALT   =>  Alt+Shift+H, Alt+K
-                // alt shift SHIFT ALT   =>   Alt+Shift
-                // alt shift [ALT] SHIFT   =>   Alt+Shift
-                // ctrl alt shift [ALT] SHIFT CTRL =>  Ctrl+Alt+Shift
-                // ctrl alt shift [ALT] SHIFT alt [ALT] CTRL =>  Ctrl+Alt+Shift   Ctrl+Alt
-                // => output on the first released key after we had a pressed key
-                if (lastPressedKey != null)
-                {
-                    string keyStr = modifiers.Contains(lastPressedKey.Value) ? "" : names[lastPressedKey.Value];
-                    for (int i = modifiers.Length - 1; i >= 0; i--)
-                        if (down[(int) modifiers[i]])
-                            keyStr = names[modifiers[i]] + (keyStr.Length == 0 ? "" : "+") + keyStr;
-                    settings.ComboCounts.IncSafe(keyStr);
-                    if (settings.RecordChords)
-                    {
-                        if (previous != null)
-                            settings.ChordCounts.IncSafe(previous + ", " + keyStr);
-                        previous = keyStr;
-                    }
-                }
-                lastPressedKey = null;
-                down[ikey] = false;
-                var downFor = DateTime.UtcNow - downAt[ikey];
-                if (downFor < TimeSpan.FromMinutes(1)) // just make sure the data is sane
-                    settings.DownFor.IncSafe(names[key], downFor.TotalSeconds);
-            };
-
-            var saver = new Thread(new ThreadStart(() =>
-            {
-                while (true)
-                {
-                    GenerateReport(settings);
-                    for (int i = 0; i < 12; i++)
-                    {
-                        settings.SaveQuiet();
-                        Thread.Sleep(TimeSpan.FromMinutes(5));
-                        settings.RuntimeMinutes += 5;
-                    }
-                }
-            }));
-            saver.IsBackground = true;
-            saver.Start();
+            new Thread(new ThreadStart(StatsSaverThread)) { IsBackground = true }.Start();
 
             Application.Run();
         }
 
-        private static void GenerateReport(Settings stats)
+        /// <summary>
+        /// The thread that periodically dumps the stats to disk and generates the report from time to time.
+        /// </summary>
+        static void StatsSaverThread()
         {
-            try
+            // Don't generate the report straight away, to make it less expensive to start at boot time
+            Thread.Sleep(TimeSpan.FromMinutes(5));
+            _stats.CountMinutes(5);
+            while (true)
             {
-                using (var file = new StreamWriter(File.Open(stats.Attribute.GetFileName() + ".report.txt", FileMode.Create, FileAccess.Write, FileShare.Read)))
+                // Generate the report once an hour
+                _stats.GenerateReport();
+                // Save the stats every 5 minutes
+                for (int i = 0; i < 12; i++)
                 {
-                    file.WriteLine("=== KEY USAGE ===");
-                    foreach (var line in stats.KeyCounts.OrderByDescending(kvp => kvp.Value).Select(kvp => "  {0,15} {1,7:0,0}".Fmt(kvp.Key, kvp.Value)))
-                        file.WriteLine(line);
-                    file.WriteLine();
-                    file.WriteLine("=== COMBO USAGE ===");
-                    foreach (var line in stats.ComboCounts.OrderByDescending(kvp => kvp.Value).Select(kvp => "  {0,15} {1,7:0,0}".Fmt(kvp.Key, kvp.Value)))
-                        file.WriteLine(line);
-                    file.WriteLine();
-                    file.WriteLine("=== CHORD USAGE ===");
-                    foreach (var line in stats.ChordCounts.OrderByDescending(kvp => kvp.Value).Select(kvp => "  {0,15} {1,7:0,0}".Fmt(kvp.Key, kvp.Value)))
-                        file.WriteLine(line);
-                    file.WriteLine();
-                    file.WriteLine("=== KEY DOWN DURATION ===");
-                    foreach (var line in stats.DownFor.OrderByDescending(kvp => kvp.Value).Select(kvp => "  {0,15} {1,7:0,0} seconds".Fmt(kvp.Key, kvp.Value)))
-                        file.WriteLine(line);
+                    _stats.Save();
+                    Thread.Sleep(TimeSpan.FromMinutes(5));
+                    _stats.CountMinutes(5);
                 }
             }
-            catch { }
+        }
+
+        /// <summary>
+        /// The callback for the keyboard hook.
+        /// </summary>
+        static int HookProc(int code, int wParam, ref WinAPI.KeyboardHookStruct lParam)
+        {
+            if (code >= 0 && lParam.vkCode >= 0 && lParam.vkCode <= 255)
+            {
+                if (wParam == WinAPI.WM_KEYDOWN || wParam == WinAPI.WM_SYSKEYDOWN)
+                    ProcessKeyDown((Key) lParam.vkCode); // lParam.scanCode
+                else if (wParam == WinAPI.WM_KEYUP || wParam == WinAPI.WM_SYSKEYUP)
+                    ProcessKeyUp((Key) lParam.vkCode); // lParam.scanCode
+            }
+            return WinAPI.CallNextHookEx(_hook, code, wParam, ref lParam);
+        }
+
+        /// <summary>
+        /// Handles the "key down" event, which is also sent for repeat keys.
+        /// </summary>
+        static void ProcessKeyDown(Key key)
+        {
+            var ikey = (byte) key;
+            if (!_keyDown[ikey])
+            {
+                _keyDownAt[ikey] = DateTime.UtcNow;
+                if (KeyCombo.IsModifier(key))
+                    _lastPressedModifier = key;
+                else
+                {
+                    _lastPressedModifier = null;
+                    ProcessKeyCombo(new KeyCombo(key, _keyDown));
+                }
+            }
+            _keyDown[ikey] = true;
+        }
+
+        /// <summary>
+        /// Handles the "key up" event.
+        /// </summary>
+        static void ProcessKeyUp(Key key)
+        {
+            if (_lastPressedModifier != null)
+            {
+                ProcessKeyCombo(new KeyCombo(_lastPressedModifier.Value, _keyDown));
+                _lastPressedModifier = null;
+            }
+
+            var ikey = (byte) key;
+            if (_keyDown[ikey])
+            {
+                _keyDown[ikey] = false;
+                _stats.CountKey(key, DateTime.UtcNow - _keyDownAt[ikey]);
+            }
+        }
+
+        static void ProcessKeyCombo(KeyCombo combo)
+        {
+#if DEBUG
+            Debug.Print(combo.ToString());
+#endif
+            _stats.CountCombo(combo);
         }
     }
 
-    [Settings("InputFrequency", SettingsKind.UserSpecific)]
-    class Settings : SettingsBase
-    {
-        public bool RecordChords = false;
-        public int RuntimeMinutes = 0;
-        public int TooLarge = 0;
-        public Dictionary<string, int> KeyCounts = new Dictionary<string, int>();
-        public Dictionary<string, int> ComboCounts = new Dictionary<string, int>();
-        public Dictionary<string, int> ChordCounts = new Dictionary<string, int>();
-        public Dictionary<string, double> DownFor = new Dictionary<string, double>();
-    }
+    /*
+     * Test cases for combo handling:  (keydown KEYUP)
+     * 
+     * alt shift [h] H SHIFT ALT   => Alt+Shift+H
+     * alt shift [h] [g] H ctrl [G] CTRL ALT SHIFT   =>  Alt+Shift+H   Alt+Shift+G   Alt+Shift+Ctrl
+     * alt shift [h] H SHIFT [k] K ALT   =>  Alt+Shift+H, Alt+K
+     * alt shift [SHIFT] ALT   =>   Alt+Shift
+     * alt shift [ALT] SHIFT   =>   Alt+Shift
+     * shift alt [ALT] SHIFT   =>   Shift+Alt
+     * ctrl alt shift [ALT] SHIFT CTRL =>  Ctrl+Alt+Shift
+     * ctrl alt shift [ALT] SHIFT alt [ALT] CTRL =>  Ctrl+Alt+Shift   Ctrl+Alt
+     */
 }
